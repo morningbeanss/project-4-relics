4018122fd8a40e9900a51fb76e305c577e2efc03
morningbeanss <clrbear150@gmail.com>
2026-05-19 13:52:56 -0700

    i'm pretty sure all the stuff RelicBuilder needs is done ?

--- Assets/Scripts/Relics/RelicBuilder.cs
+++ Assets/Scripts/Relics/RelicBuilder.cs
@@ -10,11 +10,12 @@ using Newtonsoft.Json;
 public static class RelicBuilder
 {
     static List<Relic.RelicData> relics; //list to hold all possible relics from json
-    static List<string> ownedRelics = new List<string>(); //list to hold all the relics player owns (so no dupes r presented)
+    static List<Relic.RelicData> ownedRelics = new List<Relic.RelicData>(); //list to hold all the relics player owns (so no dupes r presented)
     //make a list that temporary holds relics while they're being generated??
     //(build will be called 3 times in a row, ownedRelics shouldnt be updated during those 3 times yet
     //bc player hasnt actually selected the three. after player selects, temp one should be wiped, and selected
     //one should be officially added to ownedRelics)
+    static List<Relic.RelicData> tempRandomSelected = new List<Relic.RelicData>();
 
     static RelicBuilder()
     {
@@ -25,7 +26,7 @@ public static class RelicBuilder
     public static Relic Build() //returns randomly selected relic from relics list; no dupes
     {
         //this generates a list of all possible relics that the player DOESN'T own
-        List<Relic.RelicData> availableRelics = relics.FindAll(r => !ownedRelics.Contains(r.name));
+        List<Relic.RelicData> availableRelics = relics.FindAll(r => !tempRandomSelected.Contains(r));
         if (availableRelics.Count == 0)
         {
             Debug.Log("out of relics! (player owns all of them!)");
@@ -34,12 +35,12 @@ public static class RelicBuilder
 
         //randomly choose a relic from the available ones
         int randomIndex = Random.Range(0, availableRelics.Count);
+        Relic.RelicData selected = availableRelics[randomIndex];
+        //add it to temp holding variable
+        tempRandomSelected.Add(selected);
 
-        //i was going to add the randomly selected relic to ownedRelics, but then realized
-        //that it should only be added after the player selects which one they want :p
-        
         //return new relic
-        return new Relic(availableRelics[randomIndex]);
+        return new Relic(selected);
     }
 
     //this is similar to the one above, but in case we want to test specific relics n not a randomly selected one
@@ -56,9 +57,21 @@ public static class RelicBuilder
         }
     }
 
-    //this to add relic to owned relic, and to wipe temporary holder (void bc it shouldnt need to return anything)
-    public void SelectedRelic(string relicName)
+    //this to add relic to owned relic, and to wipe temporary holder (void bc i dont think a return is necessary)
+    // !!!! call this wherever the button selection logic is !!!!
+    public static void PlayerSelectedARelic(Relic selectedRelic)
     {
+        //get the RelicData from the Relic
+        Relic.RelicData selectedData = relics.Find(r => r.name == selectedRelic.name);
 
+        //add to owned relics (w/ if-statement for safety)
+        if (selectedData.name != null)
+        {
+            ownedRelics.Add(selectedData);
+            Debug.Log("Relic " + selectedData.name + " successfully added to player's ownedRelics");
+        }
+
+        //clear random selection
+        tempRandomSelected.Clear();
     }
 }
